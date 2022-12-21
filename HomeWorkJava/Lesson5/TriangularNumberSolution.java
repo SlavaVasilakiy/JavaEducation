0be76fdb5ce113d33ec044df79d918c95fdcdc0f
vasilakiy <slk55slk@gmail.com>
2022-12-21 16:19:56 +0300

    Java Home Work 5

--- HomeWorkJava/Lesson5/TriangularNumberSolution.java
+++ HomeWorkJava/Lesson5/TriangularNumberSolution.java
@@ -1,6 +1,5 @@
 public class TriangularNumberSolution {
     public int triangularNumberSolution(int n) {
-        int t = (n + 1) * n / 2;
-        return t;
+        return (n + 1) * n / 2;
     }
 }
