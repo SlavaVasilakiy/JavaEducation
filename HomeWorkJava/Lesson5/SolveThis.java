95868e7ca0b2b8624109c5d6c00c51fdcde720e6
vasilakiy <slk55slk@gmail.com>
2022-12-21 15:40:49 +0300

    Java Home Work 5

---
+++ HomeWorkJava/Lesson5/SolveThis.java
@@ -0,0 +1,13 @@
+package Lesson5;
+
+import java.util.Scanner;
+
+public class SolveThis {
+    public void solveThis () {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        TriangularNumberSolution mySolution = new TriangularNumberSolution();
+        int result = mySolution.triangularNumberSolution(n);
+        System.out.printf("Triangular number of %d --> %d",n, result);
+    }
+}
--- HomeWorkJava/Lesson5/TriangularNumber.java
+++ HomeWorkJava/Lesson5/TriangularNumber.java
@@ -1,5 +1,8 @@
-class TriangularNumber {
+package Lesson5;
+public class TriangularNumber {
     public static void main(String[] args) {
-
+        System.out.print("Enter a number: ");
+        SolveThis start = new SolveThis();
+        start.solveThis();
     }
 }
